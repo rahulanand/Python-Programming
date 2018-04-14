5651bfaef600768801d00b072bd1050f629a6181
rahulanand4110 <rahulanand4110@gmail.com>
2018-04-14 10:16:56 +0530

    new_out = str(out) if len(new_out) > 1: 	sum = sum_str(new_out) 	print(sum) else: 	print(new_out)

--- find_reduced_string.py
+++ find_reduced_string.py
@@ -1,8 +1,22 @@
+'''
+		Input format:
+			n = '1546287'
+			threshold = 10  "value to be compared"
+		Output format:
+			: 6
+	Explanation:
+		1.sum = 1+5+4+6 > threshold = 16
+			reduced_string = '16'287
+		2. sum = 2+8+7 > 16 (new threshold)
+		   reduced_string = 1617 , threshold = 17
+		3. sum = 1+6+1+7 = 15 < 17
+		 so reduced_string = 1+5 = 6
+'''
 #input string ex: '123456'
 n = input()
 #threshold value input
 threshold = int(input())
-
+#this method give you the red_string and new threshold value
 def find_sum(n, threshold):
 	sum = 0
 	red_string = ''
@@ -14,6 +28,7 @@ def find_sum(n, threshold):
 		 	sum = 0
 	return red_string, threshold
 
+# it will give you two digit reduced_string
 def find_reduced_string(n, threshold):
 	x = sum_str(n)
 	if x > threshold:
@@ -31,8 +46,11 @@ def sum_str(st):
 
 
 out = find_reduced_string(n, threshold)
-if len(str(out)) > 1:
-	sum = sum_str(out)
+# converting out to string
+new_out = str(out)
+#checking the length of string
+if len(new_out) > 1:
+	sum = sum_str(new_out)
 	print(sum)
 else:
-	print(out)
+	print(new_out)
