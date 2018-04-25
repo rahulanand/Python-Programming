37f490af07b8e5be0e537912337501d104991513
rahulanand4110 <rahulanand4110@gmail.com>
2018-04-25 13:39:44 +0530

    Shortest way to find the longest Word in a list

---
+++ find_longest_word_short_way.py
@@ -0,0 +1,13 @@
+#find the length of longest word from a file
+
+file_obj = open('word.txt', 'r')
+
+file_op = file_obj.read()
+
+words = file_op.split('\n')
+
+sort_word = sorted(words, key=len)
+
+longest_word = sort_word[-1]
+
+print(longest_word)
